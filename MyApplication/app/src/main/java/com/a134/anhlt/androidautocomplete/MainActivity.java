c7546b848f8a8dd9c50b400c92d29e4e7a91c7a5
andy134 <tuananh01499@gmail.com>
2016-07-07 00:41:12 +0700

    ds

---
+++ MyApplication/app/src/main/java/com/a134/anhlt/androidautocomplete/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a134.anhlt.androidautocomplete;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
